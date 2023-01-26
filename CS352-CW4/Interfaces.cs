82ab7a34b5e0bb0ea1e0fb802fe41c6c09551826
Jackson Horton <jacrhort@ut.utm.edu>
2023-01-26 14:14:21 -0600

    Add project files.

---
+++ CS352-CW4/Interfaces.cs
@@ -0,0 +1,54 @@
+﻿using CS352_CW4;
+using System;
+
+namespace CS352_CW4
+{
+    /*
+     * Interfaces
+     */
+    public interface IPhoneFactory : ISmart, IDumb
+    {
+        public ISmart GetSmart();
+        public IDumb GetDumb();
+    }
+    public interface ISmart
+    {
+        public string getName();
+    }
+
+    public interface IDumb
+    {
+        public string getName();
+    }
+
+
+    /*
+     * enum
+     */
+    public enum Manufacturers
+    {
+        SAMSUNG,
+        HTC,
+        NOKIA
+    }
+
+
+    /*
+     * Factories
+     */
+    public class SamsungFactory : IPhoneFactory
+    {
+        public ISmart GetSmart() { return null; }
+        public IDumb GetDumb() { return null; }
+    }
+    public class HTCFactory : IPhoneFactory
+    {
+        public ISmart GetSmart() { return null; }
+        public IDumb GetDumb() { return null; }
+    }
+    public class NokiaFactory : IPhoneFactory
+    {
+        public ISmart GetSmart() { return null; }
+        public IDumb GetDumb() { return null; }
+    }
+}
---
+++ CS352-CW4/PhoneTypeChecker.cs
@@ -0,0 +1,47 @@
+﻿using CS352_CW4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS352_CW4
+{
+    public class PhoneTypeChecker
+    {
+        public IPhoneFactory factory;
+        public Manufacturers manu;
+
+        public PhoneTypeChecker(Manufacturers m)
+        {
+            manu = m;
+        }
+
+        public void CheckProducts()
+        {
+            // sets member variable factory to a IPhoneFactory based on enum given
+            switch (manu)
+            {
+                case Manufacturers.SAMSUNG:
+                    factory = new SamsungFactory();
+                    break;
+                case Manufacturers.HTC:
+                    factory = new HTCFactory();
+                    break;
+                case Manufacturers.NOKIA:
+                    factory = new NokiaFactory();
+                    break;
+            }
+
+            // Checks each product from factory and write to console
+            Console.WriteLine(factory.GetDumb() + "\n" + factory.GetSmart());
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
